58ef0d170ab6468d5dc84b2258dcb3659a9506ce
lrnavarro <linmery.navarro@gmail.com>
2019-03-21 00:32:12 -0400

    modified models and migrated data to database

--- core/models.py
+++ core/models.py
@@ -1,16 +1,27 @@
 from django.db import models
+from django.urls import reverse
 # from slugger import AutoSlugField
 
 # Create your models here.
+
+
+class Author(models.Model):
+    """Model representing user making a comment, create a post and vote"""
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Post(models.Model):
     """Model representing a travel or vacation website."""
-    site_name = models.CharField(max_length=200, default='N/A', help_text='Enter an article link to a vacation destination.')
-    destination = models.CharField(max_length=200, default='N/A', help_text='Enter the destination')
-    author = models.CharField(max_length=200, default='N/A', help_text='Enter the author of the article.')
-    description = models.TextField(max_length=1000, default='N/A', help_text='Enter a description of the article.')
+    site_name = models.CharField(max_length=200, default='N/A')
+    destination = models.CharField(max_length=200, default='N/A')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    description = models.TextField(max_length=1000, default='N/A')
     date_added = models.DateTimeField(auto_now_add=True)
-    uRL = models.TextField(max_length=1000, default='N/A', help_text='Enter the URL of the article.')
-    posted_by = models.TextField(max_length=200, default='N/A', help_text='Enter the name of the person who is posting.')
+    url = models.TextField(max_length=1000, default='N/A')
     # slug = AutoSlugField(unique=True, max_length=200, populate_from='site_name')
 
     
@@ -20,9 +31,27 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_list", kwargs={"pk": self.pk})
-
+ 
 class Comment(models.Model):
-    user_comment = models.TextField(max_length=1000)
+    """Model representing user making a comment on a post"""
+    comment = models.CharField(max_length=2000)
+    commenter = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user_comment
+        return self.comment
+
+class Vote(models.Model):
+    """Model representing a user voting on a post"""
+    voter = models.ForeignKey(Author, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.voter + self.post
+
+
+
+
+
+
+
